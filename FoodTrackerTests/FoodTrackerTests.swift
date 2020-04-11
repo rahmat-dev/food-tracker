eb3934b0a4f8d4120f11d2067ee74e150002a38b
Rahmat Sulistio <rr7465457@gmail.com>
2020-04-12 00:18:34 +0700

    add unit testing for meal's data model

---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+
+    //MARK: Meal Class Tests
+    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let highestRatingMeal = Meal.init(name: "Highest Rating", photo: nil, rating: 5)
+        XCTAssertNotNil(highestRatingMeal)
+    }
+    
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 3)
+        XCTAssertNil(emptyStringMeal)
+        
+        // Rating exceeds maximum
+        let overLargeRatingMeal = Meal.init(name: "Over large", photo: nil, rating: 6)
+        XCTAssertNil(overLargeRatingMeal)
+    }
+    
+}
