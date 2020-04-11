874c1f6d9bd80b520f289ea89d284e9893939030
Rahmat Sulistio <rr7465457@gmail.com>
2020-04-11 22:01:05 +0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Rahmat Sulistio on 11/04/20.
+//  Copyright © 2020 Rahmat Fam's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
